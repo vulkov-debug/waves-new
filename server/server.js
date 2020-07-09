5a4b17b512bac7e2004e74d181805c81a612eb6f
vulkov.debug <computercenter@abv.bg>
2020-07-09 16:13:51 +0300

    fix server path

--- server/server.js
+++ server/server.js
@@ -429,9 +429,9 @@ app.post('/api/site/site_data',auth,admin,(req,res)=>{
 //Default
 if(process.env.NODE_ENV === 'production'){
      const path= require('path');
-     app.get('/*',(req,res)=>{
-        res.sendFile(path.resolve(__dirname,'../client','build','index.html'))
-     })
+     app.get((req,res)=>{
+        res.sendFile(path.join(__dirname,'../','client','build','index.html'));
+     });
 }
 
 // if (process.env.NODE_ENV === "production") {
